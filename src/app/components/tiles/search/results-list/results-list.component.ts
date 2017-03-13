195232edf8c51f7359d681edadc133f6fd101896
Your Name <you@example.com>
2017-03-13 00:18:56 -0500

    Basic song DnD functional.

--- src/app/components/tiles/search/results-list/results-list.component.ts
+++ src/app/components/tiles/search/results-list/results-list.component.ts
@@ -16,38 +16,32 @@ import { CollectionModficationData } from '../../../../models/collection-modific
 })
 export class ResultsListComponent extends TileBase {
 	items: Song[];
-	highlightClass: string[];
 
 	constructor(protected store: Store<fromRoot.State>, private collectionIndex: number) {
 		super(store,collectionIndex);
 		store.select('song').subscribe((newSongs: Song[][]) => {
 			this.items = newSongs[collectionIndex];
 			this.highlightClass = <string[]>Array(this.items.length);
-			this.items = [new Song('d','d',''),new Song('j','j',' ')];
-		});
-		store.select('draggedItemData')
-			.subscribe((draggedItemData: CollectionModficationData[]) => {
-				const myDraggedItemPaths = draggedItemData
-					.filter(item => item.collectionKey == "song")
-					.filter(item => item.collectionIndex == this.collectionIndex)
-					.map(item => this.highlightClass[item.path[0]] = "selected");
-				console.log(this.highlightClass);
-				// this.highlightClass = this.highlightClass.map((className: string, index: number) => {
-				// 	return "l"
-				// })
-			});
+			// if (this.items.length == 0) {
+			// 	this.items = Array.from(new Array(10),() => new Song('','',''));
+			// }
+				console.log(this.items);
 
+		});
 	}
 
-	onMouse(e: MouseEvent) {
+	onMouse(e: MouseEvent, ignoreIfHaveItems: boolean) {
+		// if (ignoreIfHaveItems && this.items.length > 0) {
+		// 	console.log("ignroed")
+		// 	return false;
+		// }
 		const songDragInfo = new CollectionModficationData();
 		songDragInfo.collectionKey = "song";
 		songDragInfo.collectionIndex = this.collectionIndex;
 		songDragInfo.path = [this.indexOf(e)];
 		if (isNaN(songDragInfo.path[0])) {
-			songDragInfo.path = [0];
+			songDragInfo.path = [this.items.length];
 		}
-		console.log(e)
 		if (e.type == "mousedown") {
 			this.store.dispatch(new app.DragAction(songDragInfo));
 		} else {
--- src/app/reducers/search.reducer.ts
+++ src/app/reducers/search.reducer.ts
@@ -87,7 +87,6 @@ export function reducer(state = initialState, action: app.Actions): State {
 					return prev
 				},[])
 
-			console.log(draggedItems);
 			if (sourceCollectionKey == "tray") {
 				console.log("About to make new collections for tile")
 				draggedItems[0].requiredCollectionKeys.map((key: string) => {
@@ -107,36 +106,16 @@ export function reducer(state = initialState, action: app.Actions): State {
 				state.draggedItemData.map(itemData => state[sourceCollectionKey][itemData.collectionIndex].splice(itemData.path[0],1));
 			}
 
-			console.log("determine if we should clear selection")
-			const isNoChangeSongDrag = sourceCollectionKey == "song"
-				&& data.collectionKey == "song"
-				&& (state.draggedItemData[state.draggedItemData.length-1].collectionIndex
-				== data.collectionIndex
-				&& state.draggedItemData[state.draggedItemData.length-1].path[0]
-				== data.path[0]);
 			const isTraySelfDrag = sourceCollectionKey == "tray" && data.collectionKey == "tray";
-			console.log("fate is")
-			console.log(sourceCollectionKey)
-			console.log(data.collectionKey)
 			const keysEqual = sourceCollectionKey == data.collectionKey
-			console.log(keysEqual)
-			console.log(state.draggedItemData[state.draggedItemData.length-1].collectionIndex)
-			console.log(data.collectionIndex)
 			const indicesEqual = state.draggedItemData[state.draggedItemData.length-1].collectionIndex == data.collectionIndex;
-			console.log(indicesEqual);
-			console.log(state.draggedItemData[state.draggedItemData.length-1].path[0])
-			console.log(data.path[0])
 			const pathsEqual = state.draggedItemData[state.draggedItemData.length-1].path[0] == data.path[0];
-			console.log(pathsEqual)
-			const _isNoChangeSongDrag = keysEqual && indicesEqual && pathsEqual;
-			console.log(_isNoChangeSongDrag);
-			if (!_isNoChangeSongDrag) {
+			const isNoChangeSongDrag = keysEqual && indicesEqual && pathsEqual;
+			if (!isNoChangeSongDrag) {
 				console.log("clearing dragged items")
 				state.draggedItemData = [];
-			} else {
-				console.log("what")
 			}
-			console.log(state.draggedItemData);
+
 			const newState = {};
 			newState[data.collectionKey] = [];
 			newState[sourceCollectionKey] = [];
